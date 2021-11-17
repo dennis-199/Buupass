355a886d700dce6e00836a0fb5c814d286c6f0ee
dennis-199 <otieno.dennis@strathmore.edu>
2021-11-17 12:23:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/buupass/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.buupass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.alterac.blurkit.BlurLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
