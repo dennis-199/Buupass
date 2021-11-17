ae5b939cda2fe01afa865de0a0a8c4e681146323
dennis-199 <otieno.dennis@strathmore.edu>
2021-11-17 12:36:47 +0300

    second commit

--- app/src/main/java/com/example/buupass/MainActivity.java
+++ app/src/main/java/com/example/buupass/MainActivity.java
@@ -7,13 +7,26 @@ import android.os.Bundle;
 import io.alterac.blurkit.BlurLayout;
 
 public class MainActivity extends AppCompatActivity {
+    BlurLayout blurLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        blurLayout = findViewById(R.id.blurLayout);
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        blurLayout.startBlur();
+    }
+
+    @Override
+    protected void onStop() {
+        blurLayout.pauseBlur();
+        super.onStop();
     }
 
 }
